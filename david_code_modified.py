23ba555b3a5a78773ab3cd2594064a2891c154cf
cjr <claus@rohde-hh.com>
2018-10-02 08:29:08 -0700

    reading available courses from database; output function optimized; generation of french class modified to meet requirements

--- david_code_modified.py
+++ david_code_modified.py
@@ -11,45 +11,10 @@ from concurrent.futures     import ThreadPoolExecutor
 from itertools              import permutations
 import os
 
-module_commands = ["SELECT StudentID FROM modulwahl WHERE D1 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE D2 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE D3 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE D4 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE D5 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE D6 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE D7 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE D8 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE D9 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE D10 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE E1 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE E2 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE E3 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE E4 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE E5 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE E6 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE E7 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE E8 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE E9 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE E10 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE M1 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE M2 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE M3 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE M4 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE M5 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE M6 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE M7 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE M8 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE M9 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE M10 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE F1 = 1 OR F2 = 1 OR F3 = 1 OR F4 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE S1 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE S2 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE S3 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE S4 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE S5 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE S6 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE S7 = 1;",
-                   "SELECT StudentID FROM modulwahl WHERE S8 = 1;"]
+classes_command = ["SELECT * FROM available_courses ORDER BY type;"]
+module_commands = ["SELECT StudentID FROM modulwahl WHERE {0} = 1;"]
+available_courses = []
+course_dict = dict()
 
 def call_storedproc_in_db(connection: object, procname: str, *args: tuple) -> list:
     results = None
@@ -192,7 +157,7 @@ def execute_db_command(connection: object, command: str, *args: tuple) -> list:
     return results
 
 
-def modulliste(connection: object, modul_id: int) -> list:
+def modulliste(connection: object, modul_id: str) -> list:
     """
     Builds the list of all students who selected ``modul_id``.
 
@@ -201,7 +166,7 @@ def modulliste(connection: object, modul_id: int) -> list:
     """
     global module_commands;
 
-    return [id[0] for id in execute_db_command(connection, module_commands[modul_id])]
+    return [id[0] for id in execute_db_command(connection, module_commands[0].format(modul_id))]
 
 
 def schuelerliste(connection: object) -> list:
@@ -212,7 +177,6 @@ def schuelerliste(connection: object) -> list:
     """
     query = "SELECT StudentID FROM modulwahl;"
     return [id[0] for id in execute_db_command(connection, query)]
-
 
 def zuteilen(source: set, students: set) -> list:
     """
@@ -259,7 +223,6 @@ def zuteilen(source: set, students: set) -> list:
             # Rueckgabe der Kurslisten
     return picked
 
-
 def connect_to_db() -> object:
     """
     Attempts to establish database connection.
@@ -282,23 +245,78 @@ def connect_to_db() -> object:
 
     return connection
 
+def connect_to_db2() -> object:
+    """
+    Attempts to establish database connection.
 
-def get_modules(connection: object, **kwargs: dict) -> dict:
-    laengen = [ kwargs['deutsch'], kwargs['english'], kwargs['mathe'], kwargs['franz'], kwargs['segels'] ]
-    prefixes = ['D', 'E', 'M', 'F', 'S']
+    :return: A MySQLConnection object if connection to the DB succeeds, otherwise None.
+    """
+    connection = None
 
-    modullisten = {'D': [], 'E': [], 'M': [], 'F': [], 'S': []}
-    # modullisten (student_ids, modul_name)
+    try:
+        if 'limoinput_connection_new' in environ:
+            config = loads(environ['limoinput_connection_new'])
+            connection = connect(**config)
+            print("Verbindung erfolgreich\n")
 
-    cmd_selector = 0
+        else:
+            print('SQL connection string not found!')
+
+    except Exception as e:
+        print(f"Verbindung fehlgeschlagen!\n\tReason:{str(e)}")
+
+    return connection
+
+def get_modules(connection: object, connection2: object, **kwargs: dict) -> (dict,list):
+
+    #load the list of available courses from the database
+    global classes_command
+    global available_courses
+    global course_dict
+
+    available_courses = execute_db_command(connection2, classes_command[0])
+
+    prefixes = []
+    laengen = []
+    modullisten = dict([])
+    modul_ids = []
+
+    #go through the course list and create the list with prefixes 
+    #and the number of courses of the same type
+    #create the empty dictionary 'modullisten' based on the course list
+    #create a dictionary 'course_dict' to translate between course name and ID
+    if available_courses:
+        last_prefix = available_courses[0][1]
+        last_length = 0
+        for acourse in available_courses:
+            course_name = '{}{}'.format(acourse[1], acourse[2])
+            course_dict[course_name] = acourse[0]
+            if last_prefix == acourse[1]:
+                last_length += 1
+            else:
+                prefixes.append(last_prefix)
+                laengen.append(last_length)
+                modullisten[last_prefix] = []
+                last_prefix = acourse[1]
+                last_length = 1
+        prefixes.append(last_prefix)
+        laengen.append(last_length)
+        modullisten[last_prefix] = []
+    else:
+        exit(1)
+
+    #modullisten = {'D': [], 'E': [], 'F': [], 'M': [], 'S': []}
+
     for length, prefix in zip(laengen, prefixes):  
-        for cur in range(length):  					
-            modullisten[prefix].append((modulliste(connection, cmd_selector), '{}{}'.format(prefix, cur + 1)))
-            cmd_selector = cmd_selector + 1
+        if length > 1:
+            modul_ids.append(prefix)
 
+            for cur in range(length):  					
+                modullisten[prefix].append((modulliste(connection, '{}{}'.format(prefix, cur + 1)), '{}{}'.format(prefix, cur + 1)))
+        else:
+            del modullisten[prefix]
 
-    return modullisten
-
+    return (modullisten, modul_ids)
 
 def main() -> None:
     """
@@ -312,18 +330,21 @@ def main() -> None:
 
     try:
         connection = connect_to_db()
-        kwargs = {'deutsch': 10, 'english': 10, 'mathe': 10, 'franz': 1, 'segels': 8, 'num_sections': 9, 'num_courses': 10}
+        connection2 = connect_to_db2()
+        kwargs = {'deutsch': 10, 'english': 10, 'franz': 1, 'mathe': 10, 'segels': 8, 'num_sections': 9, 'num_courses': 10}
         start = timer()
-        result = run(connection, **kwargs)
+        result = run(connection, connection2, **kwargs)
         end = timer()
         output_results(result, connection)
         print(f'time elapsed: {end - start}')
         connection.close()
+        connection2.close()
 
 
     except Exception as e:
         print(f'Something broke ...\n\tReason:{str(e)}')
         connection.close()
+        connection2.close()
         exit(1)
 
     return None
@@ -331,15 +352,21 @@ def main() -> None:
 
 def output_results(result: tuple, connection) -> None:
     unique_classes = {}
+    format_string = ',{},{},{},{},{},\n'
 
     student_info = get_student_info(connection)
 
-    for i, temp in enumerate(result[0]):
+    #check for output folder and create if necessary
+    if not os.path.exists(f'./RESULTS/') :
+        os.mkdir(f'./RESULTS/')
+
+    for i, _ in enumerate(result[0]):
         print("*" * 75)
         print("\nBegin Section {}: ".format(i + 1))
         seen = {}
 
-        for modul in temp:
+        #command line output
+        for modul in result[0][i]:
             print(f"{modul[1]}: {modul[0]}\n\tAnzahl Schueler: {len(modul[0])}\n")
             unique_classes[modul[1]] = modul[1]
 
@@ -352,37 +379,38 @@ def output_results(result: tuple, connection) -> None:
 
         print("*" * 75)
 
-    test = list(unique_classes)
-    test.sort()
-    print(f'Unique courses assigned: {test}')
-    format_string = ',{},{},{},{},{},\n'
-
-    if not os.path.exists(f'./RESULTS/') :
-        os.mkdir(f'./RESULTS/')
-
-    for i, _ in enumerate(result[0]):
+        #text file output
         with open(f'./RESULTS/section{i+1}.txt', 'w') as f:
             for modul in result[0][i]:
                 f.write(f"{modul[1]}: {modul[0]}\n\tAnzahl Schueler: {len(modul[0])}\n")
             f.write(f'Unassigned students for this section ({len(result[1][i])}): {result[1][i]}')
+            f.close()
 
-        for i, _ in enumerate(result[0]):
-            with open(f'./RESULTS/zuordnung_schiene_{i+1}.csv', 'w') as f:
-                f.write(format_string.format('Schueler-Nr.','SchuelerNachname','SchuelerRufname','Klasse','Kurs'))
-                for modul in result[0][i]:
-                    for student_id in modul[0]:
-                        if str(student_id) in student_info: 
-                            f.write(format_string.format(student_id, student_info[str(student_id)][0], 
-                                                         student_info[str(student_id)][1], student_info[str(student_id)][2], modul[1]))
-                        else:
-                            f.write(format_string.format(student_id, 'Information', 'Not', 'Available', modul[1]))
-                    f.write('Anzahl Schueler: {},\n'.format(len(modul[0])))
-                    f.write(format_string.format('', '', '', '', ''))
-                
-                for student_id in result[1][i]:
-                    f.write(format_string.format(student_id, student_info[str(student_id)][0], 
-                                                     student_info[str(student_id)][1], student_info[str(student_id)][2], 'unassigned'))
-                f.write('Anzahl Schueler: {},\n'.format(len(result[1][i])))
+        #CSV file output
+        with open(f'./RESULTS/zuordnung_schiene_{i+1}.csv', 'w') as f:
+            f.write(format_string.format('Schueler-Nr.','SchuelerNachname','SchuelerRufname','Klasse','Kurs'))
+            for modul in result[0][i]:
+                for student_id in modul[0]:
+                    if str(student_id) in student_info: 
+                        f.write(format_string.format(student_id, student_info[str(student_id)][0], 
+                                                        student_info[str(student_id)][1], student_info[str(student_id)][2], modul[1]))
+                    else:
+                        f.write(format_string.format(student_id, 'Information', 'Not', 'Available', modul[1]))
+                f.write('Anzahl Schueler: {},\n'.format(len(modul[0])))
+                f.write(format_string.format('', '', '', '', ''))
+
+            f.write('Anzahl Kurse: {},\n'.format(len(result[0][i])))
+            f.write(format_string.format('', '', '', '', ''))
+
+            for student_id in result[1][i]:
+                f.write(format_string.format(student_id, student_info[str(student_id)][0], 
+                                                    student_info[str(student_id)][1], student_info[str(student_id)][2], 'unassigned'))
+            f.write('Anzahl Schueler: {},\n'.format(len(result[1][i])))
+            f.close()
+
+    test = list(unique_classes)
+    test.sort()
+    print(f'Unique courses assigned: {test}')
 
     return None
 
@@ -437,8 +465,10 @@ def pick_modul(modulliste, students, used_courses) -> int:
     return found_index
 
 
-def first_section(modullisten) -> list:
+def first_section(modullisten) -> (list, int):
     firstS = []
+    hasFrench = 0;
+
     firstS.append(([7,12,14,19,22,31,33,41,45,48,56,79,83,85,90,95,100,110,112,134],'D1'))
     firstS.append(([17,20,21,27,29,34,71,72,73,75,92,113,124,130,137,149,152,159,174,354,355],'D2'))
     firstS.append(([62,88,93,120,142,190,199,213,263,296,314,318,321,323,324,330,336,346,350],'D5'))
@@ -454,13 +484,17 @@ def first_section(modullisten) -> list:
 
     for l in firstS:
         remove_set_difference(student_list, l[0])
-        remove_set_difference(modullisten[l[1][0]][int(l[1][1]) - 1][0], l[0])
+        if l[1] == 'F1':
+            hasFrench = 1
+        else:
+            remove_set_difference(modullisten[l[1][0]][int(l[1][1]) - 1][0], l[0])
 
-    return firstS
+    return (firstS, hasFrench)
 
-def second_section(modullisten) -> list:
+def second_section(modullisten) -> (list, int):
     secondS = []
-        
+    hasFrench = 0;
+    
     secondS.append(([3,4,5,6,16,17,26,49,61,68,81,104,120,122,128,188,192,317,318,357],'D1'))
     secondS.append(([7,13,14,18,22,23,39,41,45,51,56,63,85,93,97,110,312,332,338],'D2'))
     secondS.append(([9,10,15,27,35,52,57,64,73,84,86,92,134,159,160,162,164,179,193,321,331,343],'E1'))
@@ -476,11 +510,14 @@ def second_section(modullisten) -> list:
 
     for l in secondS:
         remove_set_difference(student_list, l[0])
-        remove_set_difference(modullisten[l[1][0]][int(l[1][1]) - 1][0], l[0])
+        if l[1] == 'F1':
+            hasFrench = 1
+        else:
+            remove_set_difference(modullisten[l[1][0]][int(l[1][1]) - 1][0], l[0])
 
-    return secondS
+    return (secondS, hasFrench)
 
-def run(connection: object, **kwargs: dict) -> None:
+def run(connection: object, connection2: object, **kwargs: dict) -> None:
     """
     Program main thread for building the section lists and sending them to the database for permanent storage
 
@@ -493,22 +530,29 @@ def run(connection: object, **kwargs: dict) -> None:
         print('Invalid connection received! Terminating ...')
         exit(1)
 
+    if connection2 is None:
+        print('Invalid connection2 received! Terminating ...')
+        exit(1)
+
+
     # stp_args = ('D1')
     # call_storedproc_in_db(connection, 'get_module_list', stp_args)
 
     leftovers = [[] for i in range(kwargs['num_sections'])]
     result = [[] for i in range(kwargs['num_sections'])]
     student_set = schuelerliste(connection)
-    modullisten = get_modules(connection, **kwargs)
-    modul_ids = ['D','E','M','S']
+    modullisten, modul_ids = get_modules(connection, connection2, **kwargs)
     id_perms = list(permutations(modul_ids, len(modul_ids)))
     # 1 E, D, M pro section
-    # all french in one course
+    # all french in one course, repeat once
     french_needed = 2
-    
-    result[0] = (first_section(modullisten))
-    result[1] = (second_section(modullisten))
-    french_needed =- 1
+
+    result[0], hasFrench = (first_section(modullisten))
+    if hasFrench == 1:
+        french_needed -= 1
+    result[1], hasFrench = (second_section(modullisten))
+    if hasFrench == 1:
+        french_needed -= 1
 
     for current_section, leftover in zip(result, leftovers):
 
@@ -520,8 +564,9 @@ def run(connection: object, **kwargs: dict) -> None:
         temp_students = set(student_set)
         
         if french_needed:
-            current_section.append((modullisten['F'][0][0], modullisten['F'][0][1]))
-            remove_set_difference(temp_students, modullisten['F'][0][0])
+            french_class = modulliste(connection, 'F1')
+            current_section.append((french_class, 'F1'))
+            remove_set_difference(temp_students, french_class)
             french_needed -= 1
             courses_needed -= 1
         
